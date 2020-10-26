22d47415448c103be1717c8855417cfa2e40d6b9
Christian Stocker <me@chregu.tv>
2020-10-26 22:54:57 +0100

    add StackVariables Utils

---
+++ src/StackVariables.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Rokka\Utils;
+
+
+class StackVariables {
+
+    public static function hasSpecialChars(string $value): bool {
+        return  (preg_match('#[$/\-\#%&?]#', $value) > 0);
+    }
+}
